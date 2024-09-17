1a0d886b3360ee7b2ae67bdba0c4fc508cb23fe0
Sandor Laszlo <slo@jedlik.eu>
2024-09-17 17:28:54 +0200

    Probalgatasok

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 - `.env` fájl létrehozása (másolása)
 - `php artisan key:generate`
 - `php artisan migrate`
+
+# Backend
+`php artisan install:api`
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/alma', function () {
+    return view('alma');
+});
+
+Route::get('/korte', function () {
+    return view('korte');
+});
